d6cad7f520fe08f9b5fb0536200ea3a01703abad
trinhcuong297 <trinhcuong297@gmail.com>
2024-06-10 07:21:21 +0700

    addAuth

--- Software/Next_Dashboard/src/pages/_app.tsx
+++ Software/Next_Dashboard/src/pages/_app.tsx
@@ -1,6 +1,43 @@
 import "@/styles/globals.css";
+import '@aws-amplify/ui-react/styles.css';
+
 import type { AppProps } from "next/app";
+import { Amplify } from 'aws-amplify';
+import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
+import AuthAWS from "@/components/Auth";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "	us-east-1_SeqeBD60b",
+      userPoolClientId: "28agse0856a2p1s56br2m33m42",
+      identityPoolId: "us-east-1:8be01534-1ef5-43eb-8bef-f6811755a41d",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider>
+    <AuthAWS>
+      <main>
+        <Component {...pageProps} />;
+      </main>
+    </AuthAWS>
+  </ThemeProvider>
 }
--- Software/Next_Dashboard/src/pages/_document.tsx
+++ Software/Next_Dashboard/src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Legend - Smart Home System with AWS</title>
+        <link
+            rel="icon"
+            href="/logo.svg"
+            type="image/x-icon"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
