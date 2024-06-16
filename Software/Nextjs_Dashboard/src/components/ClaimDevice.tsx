a2eb5ccf5eafa7e0b01806be67a67c3855233583
trinhcuong297 <trinhcuong297@gmail.com>
2024-06-16 20:40:32 +0700

    ClaimDevice

---
+++ Software/Nextjs_Dashboard/src/components/ClaimDevice.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/use-toast"
+import { useUserDataContext } from "@/context/UserDataContext"
+
+const FormSchema = z.object({
+  deviceId: z.string().min(8, {
+    message: "Invalid device ID",
+  }),
+})
+
+export function ClaimDevice() {
+  const {userAttributes, setUserAttributes, userSession, setUserSession} = useUserDataContext();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      deviceId: "",
+    },
+  })
+
+  async function onSubmit(data: z.infer<typeof FormSchema>): Promise<any> {
+    toast({
+      title: `Claiming device : ${data.deviceId}`,
+      description: ""
+    })
+    try
+    {
+      const response: any = await fetch(`https://f38t8pf54l.execute-api.us-east-1.amazonaws.com/produce/claim_device?device=${data.deviceId}&user=${userSession?.username}`,
+        {
+          method: "POST"
+        }
+      )
+      if (response.ok)
+      {
+        toast({
+          title: `Claim device ${data.deviceId} successfully`,
+          className: "bg-emerald-300"
+        })
+      }
+      else {
+        throw new Error();
+      }
+    }
+    catch (err)
+    {
+      console.log(err)
+      toast({
+        title: `Failed to claim device ${data.deviceId}`,
+        className: "bg-red-500 text-white"
+      })
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+        <FormField
+          control={form.control}
+          name="deviceId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Device ID</FormLabel>
+              <FormControl>
+                <Input placeholder="Example: abcd1234" {...field} />
+              </FormControl>
+              <FormDescription>
+                
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">Submit</Button>
+      </form>
+    </Form>
+  )
+}
--- Software/Nextjs_Dashboard/src/pages/index.tsx
+++ Software/Nextjs_Dashboard/src/pages/index.tsx
@@ -15,6 +15,11 @@ import { useEffect, useState } from "react";
 import { fetchUserAttributes } from "aws-amplify/auth";
 import { useToast } from '@/components/ui/use-toast';
 import { useUserDataContext } from "@/context/UserDataContext";
+import { Button } from "@/components/ui/button";
+import { DialogHeader, DialogFooter } from "@/components/ui/dialog";
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog";
+import { ClaimDevice } from "@/components/ClaimDevice";
+import { AudioWaveformIcon, CircleCheckBigIcon, CpuIcon, ShirtIcon } from "lucide-react";
 
 export default function Home() {
   const [wss, setWss]: [any, any] = useState(null);
@@ -34,9 +39,23 @@ export default function Home() {
                   <h2 className="text-3xl font-bold tracking-tight">
                     Welcome home, Boss
                   </h2>
-                  <div className="hidden items-center space-x-2 md:flex">
-                    {/* <CalendarDateRangePicker /> */}
-                    {/* <Button>Download</Button> */}
+                  <div className="items-center space-x-2 md:flex">
+                    <Dialog>
+                      <DialogTrigger>
+                        <Button>Add device</Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>
+                            Claim your device
+                          </DialogTitle>
+                          <DialogDescription>
+                            Scan QR code on your device / Enter your device ID
+                          </DialogDescription>
+                          <ClaimDevice />
+                        </DialogHeader>
+                      </DialogContent>
+                    </Dialog>
                   </div>
                 </div>
                 <Tabs defaultValue="overview" className="space-y-4">
@@ -54,101 +73,54 @@ export default function Home() {
                       <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                           <CardTitle className="text-sm font-medium">
-                            Total Revenue
+                            Total device
                           </CardTitle>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                          >
-                            <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                          </svg>
+                          <CpuIcon className="h-4 w-4"/>
                         </CardHeader>
                         <CardContent>
-                          <div className="text-2xl font-bold">$45,231.89</div>
+                          <div className="text-2xl font-bold">100</div>
                           <p className="text-xs text-muted-foreground">
-                            +20.1% from last month
+                            All device in home
                           </p>
                         </CardContent>
                       </Card>
                       <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                           <CardTitle className="text-sm font-medium">
-                            Subscriptions
+                            Active device
                           </CardTitle>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                          >
-                            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                            <circle cx="9" cy="7" r="4" />
-                            <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                          </svg>
+                          <CircleCheckBigIcon className="w-4 h-4" />
                         </CardHeader>
                         <CardContent>
                           <div className="text-2xl font-bold">+2350</div>
                           <p className="text-xs text-muted-foreground">
-                            +180.1% from last month
+                            +1 from last hour
                           </p>
                         </CardContent>
                       </Card>
                       <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                          <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                          >
-                            <rect width="20" height="14" x="2" y="5" rx="2" />
-                            <path d="M2 10h20" />
-                          </svg>
+                          <CardTitle className="text-sm font-medium">Device health</CardTitle>
+                          <AudioWaveformIcon className="w-4 h-4" />
                         </CardHeader>
                         <CardContent>
-                          <div className="text-2xl font-bold">+12,234</div>
+                          <div className="text-2xl font-bold">98%</div>
                           <p className="text-xs text-muted-foreground">
-                            +19% from last month
+                            Almost device is healthy
                           </p>
                         </CardContent>
                       </Card>
                       <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                           <CardTitle className="text-sm font-medium">
-                            Active Now
+                            Scene
                           </CardTitle>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-4 w-4 text-muted-foreground"
-                          >
-                            <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                          </svg>
+                          <ShirtIcon className="w-4 h-4" />
                         </CardHeader>
                         <CardContent>
-                          <div className="text-2xl font-bold">+573</div>
+                          <div className="text-2xl font-bold">+67</div>
                           <p className="text-xs text-muted-foreground">
-                            +201 since last hour
+                            Rainy Scene
                           </p>
                         </CardContent>
                       </Card>
